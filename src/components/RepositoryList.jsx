ea73a64383b2dd61f83dbcfe6df2234cbef1b952
brandaoplaster <brandaoplaster@gmail.com>
2022-12-22 22:41:33 -0300

    feat: Adding component repository list

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,14 @@
+export function RepositoryList() {
+  return (
+    <section className="repository-list">
+      <h1>List Repositories</h1>
+      <ul>
+        <li>
+          <strong>unform</strong>
+          <p>Forms in reactjs</p>
+          <a href="#">Access repository</a>
+        </li>
+      </ul>
+    </section>
+  )
+}
--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
+import { RepositoryList } from "./components/RepositoryList";
 
 function App() {
-  return <h1>Hello world!</h1>
+  return <RepositoryList />
 }
