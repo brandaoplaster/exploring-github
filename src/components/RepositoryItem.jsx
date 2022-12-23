dfa6585e464b90c849ac7528d69cb2be111e58bf
brandaoplaster <brandaoplaster@gmail.com>
2022-12-23 20:09:51 -0300

    feat: adding component repository item

---
+++ src/components/RepositoryItem.jsx
@@ -0,0 +1,9 @@
+export function RepositoryItem() {
+  return (
+    <li>
+      <strong>unform</strong>
+      <p>Forms in reactjs</p>
+      <a href="#">Access repository</a>
+    </li>
+  )
+}
--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,13 +1,11 @@
+import { RepositoryItem } from "./RepositoryItem";
+
 export function RepositoryList() {
   return (
     <section className="repository-list">
       <h1>List Repositories</h1>
       <ul>
-        <li>
-          <strong>unform</strong>
-          <p>Forms in reactjs</p>
-          <a href="#">Access repository</a>
-        </li>
+        <RepositoryItem />
       </ul>
     </section>
   )
