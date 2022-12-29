fd36954c49a653170cd31b8110b02353b478b98f
brandaoplaster <brandaoplaster@gmail.com>
2022-12-28 22:47:09 -0300

    feat: Ajusting repositoryItem component

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -1,9 +1,13 @@
 export function RepositoryItem() {
   return (
     <li>
-      <strong>unform</strong>
-      <p>Forms in reactjs</p>
-      <a href="#">Access repository</a>
+      <strong>
+        {props.reposiotry.name}
+      </strong>
+      <p>
+        {props.reposiotry.description}
+      </p>
+      <a href={props.reposiotry.html_url}>Access repository</a>
     </li>
   )
 }
