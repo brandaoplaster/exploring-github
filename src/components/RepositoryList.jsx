a3344416b6e7ef42864c92f026a19879a5fc0bec
brandaoplaster <brandaoplaster@gmail.com>
2022-12-28 22:48:18 -0300

    feat: Adding call in github api

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,13 +1,23 @@
+import { useState, useEffect } from 'react';
 import { RepositoryItem } from "./RepositoryItem";
-
 import '../styles/repositories.scss';
 
 export function RepositoryList() {
+  const [repositories, setRepository] = useState([]);
+
+  useEffect(() => {
+    fetch('https://api.github.com/orgs')
+    .then(response => response.json())
+    .then(data => setRepository(data))
+  }, []);
+
   return (
     <section className="repository-list">
       <h1>List Repositories</h1>
       <ul>
-        <RepositoryItem />
+        {repositories.map(repository => {
+          return <RepositoryItem key={repository.name} repository={repository} />
+        })}
       </ul>
     </section>
   )
