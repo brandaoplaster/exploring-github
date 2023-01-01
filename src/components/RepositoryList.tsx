60bc905ad91f8d5bbc7cdc2f5d2c8560a2df5b28
brandaoplaster <brandaoplaster@gmail.com>
2022-12-31 22:10:48 -0300

    chore: Converting repositoryList component from jsx to tsx

--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -2,8 +2,14 @@ import { useState, useEffect } from 'react';
 import { RepositoryItem } from "./RepositoryItem";
 import '../styles/repositories.scss';
 
+interface Repository{
+  name: string;
+  description: string;
+  html_url: string;
+}
+
 export function RepositoryList() {
-  const [repositories, setRepository] = useState([]);
+  const [repositories, setRepository] = useState<Repository[]>([]);
 
   useEffect(() => {
     fetch('https://api.github.com/orgs')
